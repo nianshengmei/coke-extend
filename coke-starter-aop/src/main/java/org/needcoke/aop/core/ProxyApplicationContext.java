df21cbf1ae642d78027600d68053a765544534ed
warren <2410818122@qq.com>
2022-08-21 23:19:35 +0800

    新增 aop容器上下文

---
+++ coke-starter-aop/src/main/java/org/needcoke/aop/core/ProxyApplicationContext.java
@@ -0,0 +1,10 @@
+package org.needcoke.aop.core;
+
+import pers.warren.ioc.core.ApplicationContext;
+
+/**
+ * @author warren
+ * @date 2022/4/2
+ */
+public class ProxyApplicationContext extends ApplicationContext {
+}
