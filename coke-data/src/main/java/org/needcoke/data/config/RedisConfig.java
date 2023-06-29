66de442f26be01be9fd038b33715cc611885e1fb
yanming <waym@1314>
2023-06-29 22:04:53 +0800

    coke 支持cel表达式

--- coke-data/src/main/java/org/needcoke/data/config/RedisConfig.java
+++ coke-data/src/main/java/org/needcoke/data/config/RedisConfig.java
@@ -3,10 +3,14 @@ package org.needcoke.data.config;
 import lombok.Data;
 import pers.warren.ioc.annotation.Configuration;
 import pers.warren.ioc.annotation.Value;
+
 import java.util.List;
 
 /**
+ * 关于redisson的一些配置信息
  *
+ * @author warren
+ * @since 1.0.3
  */
 @Data
 @Configuration
@@ -150,6 +154,13 @@ public class RedisConfig {
     /**
      * 负载均衡算法类的选择
      *
+     * <p>默认值：org.redisson.connection.balancer.RoundRobinLoadBalancer</p>
+     *
+     * <p>在使用多个Elasticache Redis服务节点的环境里，可以选用以下几种负载均衡方式选择一个节点： </p>
+     * <p>org.redisson.connection.balancer.WeightedRoundRobinBalancer - 权重轮询调度算法 </p>
+     * <p>org.redisson.connection.balancer.RoundRobinLoadBalancer - 轮询调度算法 </p>
+     * <p>org.redisson.connection.balancer.RandomLoadBalancer - 随机调度算法</p>
+     *
      * @since 1.0.3
      */
     @Value("coke.data.redis.load-balance:org.redisson.connection.balancer.RoundRobinLoadBalancer")
@@ -171,43 +182,190 @@ public class RedisConfig {
     public static final String LB_WEIGHTED_ROUND_ROBIN_BALANCER = "org.redisson.connection.balancer.WeightedRoundRobinBalancer";
 
     /**
+     * 默认值：5000
+     * <p>
+     * 用来指定检查节点DNS变化的时间间隔。使用的时候应该确保JVM里的DNS数据的缓存时间保持在足够低的范围才有意义。用-1来禁用该功能。
+     */
+    @Value("coke.data.redis.dns-monitor-interval:5000")
+    private Long dnsMonitoringInterval;
+
+    /**
      * 从节点发布和订阅连接的最小空闲连接数
+     * <p>默认值：1
+     * <p>
+     * 多从节点的环境里，每个 从服务节点里用于发布和订阅连接的最小保持连接数（长连接）。Redisson内部经常通过发布和订阅来实现许多功能。长期保持一定数量的发布订阅连接是必须的。</p>
      *
      * @since 1.0.3
      */
+    @Value("coke.data.redis.subscription-connection-minimum-idle-size:1")
     private Integer subscriptionConnectionMinimumIdleSize;
 
+    /**
+     * 从节点发布和订阅连接池大小
+     * 默认值：50
+     * <p>
+     * 多从节点的环境里，每个 从服务节点里用于发布和订阅连接的连接池最大容量。连接池的连接数量自动弹性伸缩。
+     */
+    @Value("coke.data.redis.subscription-connection-pool-size:50")
     private Integer subscriptionConnectionPoolSize;
 
+    /**
+     * 从节点最小空闲连接数
+     * <p>默认值：32</p>
+     * <p>多从节点的环境里，每个 从服务节点里用于普通操作（非 发布和订阅）的最小保持连接数（长连接）。长期保持一定数量的连接有利于提高瞬时读取反映速度</p>
+     */
+    @Value("coke.data.redis.slave-connection-minimum-idle-size:32")
     private Integer slaveConnectionMinimumIdleSize;
 
+    /**
+     * 从节点连接池大小
+     * <p>默认值：64</p>
+     * <p>多从节点的环境里，每个 从服务节点里用于普通操作（非 发布和订阅）连接的连接池最大容量。连接池的连接数量自动弹性伸缩。</p>
+     */
+    @Value("coke.data.redis.slave-connection-pool-size:64")
     private Integer slaveConnectionPoolSize;
 
+    /**
+     * 主节点最小空闲连接数
+     * <p>
+     * 默认值：32
+     * <p>
+     * 多从节点的环境里，每个 主节点的最小保持连接数（长连接）。长期保持一定数量的连接有利于提高瞬时写入反应速度。
+     */
+    @Value("coke.data.redis.master-connection-minimum-idle-size:32")
     private Integer masterConnectionMinimumIdleSize;
 
+    /**
+     * 主节点连接池大小
+     * 默认值：64
+     * <p>
+     * 主节点的连接池最大容量。连接池的连接数量自动弹性伸缩。
+     */
+    @Value("coke.data.redis.master-connection-pool-size:64")
     private Integer masterConnectionPoolSize;
 
+    /**
+     * 连接空闲超时，单位：毫秒
+     */
+    @Value("coke.data.redis.idle-connection-timeout:10000")
     private Long idleConnectionTimeout;
 
+    /**
+     * 连接超时，单位：毫秒
+     */
+    @Value("coke.data.redis.connect-timeout:10000")
     private Long connectTimeout;
 
+    /**
+     * 命令等待超时，单位：毫秒
+     */
+    @Value("coke.data.redis.timeout:3000")
     private Long timeout;
 
+    /**
+     * 命令失败重试次数
+     *
+     * <p>默认值：3</p>
+     * <p>如果尝试达到 retryAttempts（命令失败重试次数） 仍然不能将命令发送至某个指定的节点时，将抛出错误。如果尝试在此限制之内发送成功，则开始启用 timeout（命令等待超时） 计时。</p>
+     */
+    @Value("coke.data.redis.retry-attempts:3")
     private Integer retryAttempts;
 
+    /**
+     * 命令重试发送时间间隔，单位：毫秒
+     * <p>
+     * 默认值：1500
+     * <p>
+     * 在某个节点执行相同或不同命令时，连续 失败 failedAttempts（执行失败最大次数） 时，该节点将被从可用节点列表里清除，直到 reconnectionTimeout（重新连接时间间隔） 超时以后再次尝试。
+     */
+    @Value("coke.data.redis.retry-interval:1500")
     private Long retryInterval;
 
+    /**
+     * 默认值：null
+     * <p>
+     * 用于节点身份验证的密码。
+     */
+    @Value("coke.data.redis.password")
     private String password;
 
+    /**
+     * 默认值：5
+     * <p>
+     * 每个连接的最大订阅数量。
+     */
+    @Value("coke.data.redis.subscriptions-per-connection:5")
     private Integer subscriptionsPerConnection;
 
+    /**
+     * 默认值：null
+     * <p>
+     * 在Redis节点里显示的客户端名称。
+     */
+    @Value("coke.data.redis.client-name:coke-client")
     private String clientName;
 
+    /**
+     * 默认值：true
+     * <p>
+     * 开启SSL终端识别能力。
+     */
+    @Value("coke.data.redis.ssl-enable-endpoint-identification:true")
     private Boolean sslEnableEndpointIdentification;
 
+    /**
+     * 默认值：JDK
+     * <p>
+     * 确定采用哪种方式（JDK或OPENSSL）来实现SSL连接。
+     */
+    @Value("coke.data.redis.ssl-provider:JDK")
     private String sslProvider;
 
+    public static final String SSL_PROVIDER_JDK = "JDK";
 
+    public static final String SSL_PROVIDER_OPENSSL = "OPENSSL";
 
+    /**
+     * 指定SSL信任证书库的路径。
+     */
+    @Value("coke.data.redis.ssl-truststore")
+    private String sslTruststore;
 
+    /**
+     * SSL信任证书库密码
+     */
+    @Value("coke.data.redis.ssl-truststore-password")
+    private String sslTruststorePassword;
+
+    /**
+     * SSL钥匙库路径
+     */
+    @Value("coke.data.redis.ssl-keystore")
+    private String sslKeystore;
+
+    /**
+     * SSL钥匙库密码
+     */
+    @Value("coke.data.redis.ssl-keystore-password")
+    private String sslKeystorePassword;
+
+    /**
+     * 最小空闲连接数，单节点模式
+     * <p>
+     * 默认值：32
+     * <p>
+     * 最小保持连接数（长连接）。长期保持一定数量的连接有利于提高瞬时写入反应速度。
+     */
+    @Value("coke.data.redis.connection-minimum-idle-size:32")
+    private Integer connectionMinimumIdleSize;
+
+    /**
+     * 默认值：64
+     * <p>
+     * 连接池大小
+     * <p>
+     * 单节点模式
+     */
+    @Value("coke.data.redis.connection-pool-size:64")
+    private Integer connectionPoolSize;
 }
